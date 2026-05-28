791b73168528b93c4f9a763e35bfaa4d50c466b9
nrj1007 <nrj1007@users.noreply.github.com>
2026-05-29 01:10:14 +0530

    Untrack .tmp-debug build/debug files and add to .gitignore

--- .tmp-debug/app/api/cards/route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GET = GET;
-const server_1 = require("next/server");
-const cards_1 = require("@/lib/cards");
-function GET() {
-    return server_1.NextResponse.json({ cards: cards_1.cards });
-}
