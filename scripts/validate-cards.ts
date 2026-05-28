597815d4b7b164bf228fca43ad464b4720a43024
nrj1007 <nrj1007@users.noreply.github.com>
2026-05-28 23:13:38 +0530

    Add smartbuy giftr 5X reward points category to HDFC Infinia card

--- scripts/validate-cards.ts
+++ scripts/validate-cards.ts
@@ -75,6 +75,7 @@ const allowedRewardCategories = new Set([
   "smartbuy flights",
   "smartbuy hotels",
   "smartbuy train",
+  "smartbuy giftr",
   "swiggy zomato",
   "tata brands",
   "tata neu",
